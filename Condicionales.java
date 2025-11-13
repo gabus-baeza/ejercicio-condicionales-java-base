0dc396f33875d6e0904fb3015f1bc61b4ec598d9
gabus-baeza <ggbaeza@gmail.com>
2025-11-13 11:38:28 -0600

    Create Condicionales.java with main method
    Added a new Java class for conditional statements with placeholders for future functionality.

---
+++ Condicionales.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Condicionales {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        // TODO: Solicita la edad y determina si puede votar
+        
+        // TODO: Declara dos números y muestra cuál es mayor
+        
+        // TODO: Verifica si un número es positivo, negativo o cero
+    }
+}
